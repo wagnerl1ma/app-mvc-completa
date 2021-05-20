a6c977a3ac878e077a8f0d9a5814e4ac841f9964
Wagner Lima <wagner.araujo92@outlook.com>
2021-05-20 17:09:52 -0300

    Finalizando o ajuste das Views

---
+++ MinhaAppMvcCompleta/src/DevIO.App/Extensions/RazorExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevIO.App.Extensions
+{
+    public static class RazorExtensions
+    {
+        public static string FormataDocumento(this RazorPage page, int tipoPessoa, string documento)
+        {
+            return tipoPessoa == 1 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00") : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
+        }
+
+        //public static string MarcarOpcao(this RazorPage page, int tipoPessoa, int valor)
+        //{
+        //    return tipoPessoa == valor ? "checked" : "";
+        //}
+
+        //public static bool IfClaim(this RazorPage page, string claimName, string claimValue)
+        //{
+        //    return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimName, claimValue);
+        //}
+
+        //public static string IfClaimShow(this RazorPage page, string claimName, string claimValue)
+        //{
+        //    return CustomAuthorization.ValidarClaimsUsuario(page.Context, claimName, claimValue) ? "" : "disabled";
+        //}
+
+        //public static IHtmlContent IfClaimShow(this IHtmlContent page, HttpContext context, string claimName, string claimValue)
+        //{
+        //    return CustomAuthorization.ValidarClaimsUsuario(context, claimName, claimValue) ? page : null;
+        //}
+    }
+}
