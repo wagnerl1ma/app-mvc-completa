ab8712f6d6fd92b557609c3e949298aca76c19a1
Wagner Lima <wagner.araujo92@outlook.com>
2021-06-05 13:41:13 -0300

    Tratamento de erros

--- MinhaAppMvcCompleta/src/DevIO.App/Startup.cs
+++ MinhaAppMvcCompleta/src/DevIO.App/Startup.cs
@@ -75,7 +75,8 @@ namespace DevIO.App
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/erro/500");
+                app.UseStatusCodePagesWithRedirects("/erro/{0}");
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
